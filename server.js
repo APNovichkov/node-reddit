d056c75635b2e78dce4c8e5febbaa6baf422cde2
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-09 00:06:57 -0700

    [add] Users can now submit and save posts

--- server.js
+++ server.js
@@ -6,15 +6,32 @@ const app = express();
 
 // Middleware
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+
+
+// Add controllers
+require('./controllers/posts.js')(app);
+require('./data/reddit-db');
 
 // GET Routes
 
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+})
+
+
 
 
 
