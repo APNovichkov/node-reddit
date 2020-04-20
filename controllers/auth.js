1d0b5aa7fcdb1f5579a07f5e01f4e26fb259c263
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-19 17:11:49 -0700

    [add] Users can now log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,9 +26,41 @@ module.exports = (app) => {
       });
   });
 
-  // LOGOUT 
+  // LOGOUT
   app.get('/logout', (req, res) => {
    res.clearCookie('nToken');
    res.redirect('/');
  });
+
+ // LOGIN FORM
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  app.post('/login', (req, res) => {
+      const username = req.body.username;
+      const password = req.body.password;
+
+      User.findOne({ username }, "username password")
+        .then(user => {
+            if(!user){
+                return res.status(401).send({message: 'wrong username or password'});
+            }
+
+            user.comparePassword(password, (err, isMatch) => {
+                if(!isMatch){
+                    return res.status(401).send({message: 'wrong username or password'});
+                }
+
+                const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                    expiresIn: "60 days"
+                })
+
+                res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+                res.redirect("/")
+            })
+        }).catch(err => {
+            console.log(err)
+        })
+  })
 }
