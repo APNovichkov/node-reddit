c480f64976376fb8537777371454c114e1320f4f
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-25 20:41:18 -0700

    [add] Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,15 +1,34 @@
 const Post = require('../models/post');
+const User = require('../models/user')
 
 module.exports = (app) => {
     app.get('/posts/new', (req, res) => {
-        res.render('posts-new')
+        var currentUser = req.user;
+
+        res.render('posts-new', { currentUser })
     })
 
     app.post('/posts/new', (req, res) => {
+        console.log("Submitting post")
         if(req.user){
             const post = new Post(req.body);
-            post.save((err, s) => {
-                return res.redirect(`/`);
+            post.author = req.user
+
+            console.log("User is not null")
+
+            post.save()
+            .then(post => {
+                return User.findById(req.user._id)
+            })
+            .then(user => {
+                console.log(`User id: ${req.user._id}`)
+                user.posts.unshift(post);
+                user.save()
+                res.redirect(`/posts/${post._id}`)
+            })
+            .catch(err => {
+                console.log("Error!")
+                console.log(err.message);
             })
         }else{
             res.sendStatus(401); // Unauthorized!
@@ -19,7 +38,7 @@ module.exports = (app) => {
     app.get('/', (req, res) => {
         var currentUser = req.user;
 
-        Post.find({}).lean()
+        Post.find().populate('author').lean()
             .then(posts => {
                 res.render("posts-index", { posts, currentUser });
             })
@@ -31,7 +50,7 @@ module.exports = (app) => {
     app.get("/posts/:id", function(req, res) {
         var currentUser = req.user;
 
-        Post.findById(req.params.id).populate('comments').lean()
+        Post.findById(req.params.id).populate('comments').populate('author').lean()
         .then(post => {
             res.render("posts-show", { post, currentUser });
         })
@@ -45,7 +64,7 @@ module.exports = (app) => {
     app.get("/n/:subreddit", function(req, res) {
         var currentUser = req.user;
 
-        Post.find({ subreddit: req.params.subreddit }).lean()
+        Post.find({ subreddit: req.params.subreddit }).populate('author').lean()
           .then(posts => {
             res.render("posts-index", { posts, currentUser });
           })
