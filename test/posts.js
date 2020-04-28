6847d9bc1ddd5f67889822767ffa46964ba0ae66
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-28 08:07:22 -0700

    [add] Added remaining authentication tests

--- test/posts.js
+++ test/posts.js
@@ -6,20 +6,64 @@ const expect = chai.expect;
 // Import the Post model from our models folder so we
 // we can use it in our tests.
 const Post = require('../models/post');
+const User = require('../models/user')
 const server = require('../server');
 
 chai.should();
 chai.use(chaiHttp);
 
+const agent = chai.request.agent(app);
+
+// Post that we'll use for testing purposes
+const newPost = {
+    title: 'post title',
+    url: 'https://www.google.com',
+    summary: 'post summary',
+    subreddit: 'cats'
+};
+
+const user = {
+    username: 'poststest',
+    password: 'testposts'
+};
+
+before(function (done) {
+  agent
+    .post('/sign-up')
+    .set("content-type", "application/x-www-form-urlencoded")
+    .send(user)
+    .then(function (res) {
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+});
+
+after(function (done) {
+  Post.findOneAndDelete(newPost)
+  .then(function (res) {
+      agent.close()
+
+      User.findOneAndDelete({
+          username: user.username
+      })
+        .then(function (res) {
+            done()
+        })
+        .catch(function (err) {
+            done(err);
+        });
+  })
+  .catch(function (err) {
+      done(err);
+  });
+});
+
 describe('Posts', function() {
-  const agent = chai.request.agent(server);
-  // Post that we'll use for testing purposes
-  const newPost = {
-      title: 'post title',
-      url: 'https://www.google.com',
-      summary: 'post summary'
-  };
-  it("should create with valid attributes at POST /posts/new", function (done) {
+  // const agent = chai.request.agent(server);
+
+    it("should create with valid attributes at POST /posts/new", function (done) {
       // Checks how many posts there are now
     Post.estimatedDocumentCount()
       .then(function (initialDocCount) {
